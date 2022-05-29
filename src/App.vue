84f7c8424f5876deba77d480a00cc3ee24bcb7d1
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2022-05-29 18:53:15 +0200

    refactor: init file structure

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template id="app">
+    <slider-side />
+    <login-side />
+</template>
+
+<script lang="ts">
+import SliderSide from './components/SliderComponents/SliderSide.vue'
+import LoginSide from './components/LoginComponents/LoginSide.vue'
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    components: {
+        LoginSide,
+        SliderSide,
+    },
+})
+</script>
+
+<style lang="scss">
+#app {
+    display: flex;
+}
+</style>
---
+++ src/components/SliderComponents/SliderSide.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>Side2</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+div {
+    width: 50%;
+    height: 100vh;
+    background-color: $slider-bg-color;
+}
+</style>
