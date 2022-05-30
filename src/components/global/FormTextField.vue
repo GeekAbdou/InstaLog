bb081e487babe5f72ed40aeae2aa9e0e1a8b6c03
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2022-05-30 16:31:39 +0200

    fix: :bug: Fixing Error Email/password invalid message

--- src/components/global/FormTextField.vue
+++ src/components/global/FormTextField.vue
@@ -33,7 +33,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
     name: 'form-text-field',
