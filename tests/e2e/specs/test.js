1ed92663b9ae7fca5427d719ca62cb7ea1bc98f8
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2022-05-30 17:53:40 +0200

    test: :fire: Adding more 7 e2e test cases

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -46,3 +46,74 @@ describe('Scenario 4', () => {
         )
     })
 })
+
+describe('Scenario 5', () => {
+    it('Checks for wrong password length', () => {
+        cy.visit('/login')
+        cy.get('[data-cy=password]').type('1234567')
+        cy.get('[data-cy=errorMessage]')
+            .contains('Password must be 8 characters or more.')
+            .should('have.css', 'color', 'rgb(255, 0, 0)')
+        cy.get('[data-cy=password]').should(
+            'have.css',
+            'border-color',
+            'rgb(255, 0, 0)'
+        )
+    })
+})
+
+describe('Scenario 6', () => {
+    it('Checks for disable button till email and password have accepted rules', () => {
+        cy.visit('/login')
+        cy.get('[data-cy=email]').type('mohamed@instabug.com')
+        cy.get('[data-cy=logBtn]').should('be.disabled')
+        cy.get('[data-cy=password]').type('123456')
+        cy.get('[data-cy=logBtn]').should('be.disabled')
+        cy.get('[data-cy=password]').type('78')
+        cy.get('[data-cy=logBtn]').should('be.enabled')
+    })
+})
+
+describe('Scenario 7', () => {
+    it('Checks logout functionality', () => {
+        cy.visit('/login')
+        cy.get('[data-cy=email]').type('mohamed@instabug.com')
+        cy.get('[data-cy=password]').type('A12345678')
+        cy.get('[data-cy=logBtn]').click()
+        cy.get('[data-cy=welMessage]').contains('Welcome, mohamed@instabug.com')
+        cy.get('[data-cy=logoutBtn]').click()
+        cy.url().should('include', '/login')
+    })
+})
+
+describe('Scenario 8', () => {
+    it('Checks 404 Not Found redirects', () => {
+        cy.visit('/doesnotexist')
+        cy.url().should('include', '/404')
+    })
+})
+
+describe('Scenario 9', () => {
+    it('Checks autologin', () => {
+        cy.visit('/login')
+        cy.get('[data-cy=email]').type('mohamed@instabug.com')
+        cy.get('[data-cy=password]').type('A12345678')
+        cy.get('[data-cy=logBtn]').click()
+        cy.get('[data-cy=welMessage]').contains('Welcome, mohamed@instabug.com')
+        cy.visit('/login')
+        cy.url().should('include', '/welcome')
+    })
+})
+
+describe('Scenario 10', () => {
+    it('Checks Slide Picker', () => {
+        cy.visit('/login')
+        cy.get('[data-cy=slidePicker] > span').last().click()
+        cy.get('[data-cy=slidePicker] > span')
+            .last()
+            .should(
+                'have.class',
+                'slider-section__carousel__pagination__picker--active'
+            )
+    })
+})
